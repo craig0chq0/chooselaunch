01f9441f7748daf1cb78d4ebf0d93f4bdc2814c6
craig0chq0 <705284836@qq.com>
2018-08-07 10:34:00 +0800

    初步完成

--- assets/scr/chooselaunch.ts
+++ assets/scr/chooselaunch.ts
@@ -10,35 +10,33 @@ export default class launchmain extends cc.Component {
 
 
     private fdList: string[] = [];
-    private fdlist: string[] = ["a", "b", "c", "d", "e", "f"];
+    private fdlist: string[] = ["老卤面", "安庆小吃", "兰州拉面", "黄焖鸡", "沙县小吃", "其他"];
     choose(list: string[]) {
         let ind = Math.random() * list.length | 0;
         let food = list[ind];
         let newlist: string[] = [];
         for (let i = 0; i < list.length; ++i) {
-            if (i !== ind) {
-                newlist.push(list[i])
+            if (list[i] !== food) {
+                newlist.push(list[i]);
             }
         }
-        return { Food: food, List: newlist }
-
+        return {List:newlist,Food:food};
     }
-
     btchooselist() {
         if (this.fdList.length === 0) {
             this.fdList = this.fdlist
         }
-        let ret = this.choose(this.fdList);
-        this.fdList = ret.List;
         this.list.string = `今天的菜谱是${this.fdList}`;
+        
     }
     btchoosefood() {
         if (this.fdList.length === 0) {
             this.fdList = this.fdlist
         }
-        let ret = this.choose(this.fdList);
-        // this.fdList=ret.List;
-        let food = ret.Food;
-        this.food.string = `可以吃${food}`;
+        let ret=this.choose(this.fdList);
+        this.fdList=ret.List;
+        let food=ret.Food
+        this.food.string = `可以吃${food}`; 
+        
     }
 }
